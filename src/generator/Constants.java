4f4646fc6e1c468eb8f527bb99814269993e7834
Andre Matutat <andre.matutat@fh-bielefeld.de>
2019-12-17 18:06:47 +0100

    impl randomLevelGenerator

--- src/generator/Constants.java
+++ src/generator/Constants.java
@@ -3,5 +3,5 @@ package generator;
 public class Constants {
 
 	static int POPULATIONSIZE=100;
-	
+	static int FLOORCHANCE=70;
 }
--- src/generator/LevelGenerator.java
+++ src/generator/LevelGenerator.java
@@ -3,23 +3,58 @@ package generator;
 import java.util.ArrayList;
 
 /**
- * Erstellt mithilfe eines Genetischen Algorithmus ein String[][] kodiertes level
+ * Erstellt mithilfe eines Genetischen Algorithmus ein String[][] kodiertes
+ * level
+ * 
  * @author Andre
  *
  */
-public class LevelGenerator{
-	private ArrayList <String[][]> levels = new ArrayList <String[][]>();
-	
+public class LevelGenerator {
+	private ArrayList<String[][]> levels = new ArrayList<String[][]>();
+
 	/**
 	 * Generiert ein String[][] kodiertes Level
+	 * 
 	 * @param xSize Breite des Levels
 	 * @param ySize Höhe des Levels
 	 * @return kodiertes Level
 	 */
 	public String[][] generateLevel(int xSize, int ySize) {
+		String[][] level = this.generateRandomLevel(xSize, ySize);
+
+		for (int y = 0; y < ySize; y++) {
+			for (int x = 0; x < xSize; x++) {
+				System.out.print(level[x][y]);
+			}
+			System.out.println("");
+		}
+
+		return level;
+	}
+
+	private String[][] generateRandomLevel(int xSize, int ySize) {
 		String[][] level = new String[xSize][ySize];
-				
+
+		for (int y = 0; y < ySize; y++) {
+			for (int x = 0; x < xSize; x++) {
+
+				if (x == 0 || y == 0 || y == ySize-1 || x == xSize-1) {
+					level[x][y] = "W";
+				}
+				else {
+					if((int)(Math.random()*100+1)<=Constants.FLOORCHANCE)
+						level[x][y]="F";
+					else level[x][y]="W";
+				}
+			}
+		}
+
 		return level;
 	}
 	
+	public static void main (String[]args) {
+		LevelGenerator lg = new LevelGenerator();
+		lg.generateLevel(20, 20);
+	}
+
 }
