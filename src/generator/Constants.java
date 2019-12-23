26d1f35b058e181a92ff33ad75fe8dd97789a8c1
Andre Matutat <andre.matutat@fh-bielefeld.de>
2019-12-23 01:16:04 +0100

    remove crossoverfactor

--- src/generator/Constants.java
+++ src/generator/Constants.java
@@ -4,7 +4,6 @@ public class Constants {
 
 	static int POPULATIONSIZE=100;
 	static int FLOORCHANCE=70;
-	static int CROSSOVERFACTOR=3;	
 	static int MUTATIONCHANCE=1;
 	static int CROSSOVERCHANCE=60;
 	static int MAXGENERATION = 200;
