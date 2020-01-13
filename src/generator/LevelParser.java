dca2f6ac70cca41657cea94306ebfe8b26d6d842
Andre Matutat <andre.matutat@fh-bielefeld.de>
2020-01-13 21:10:34 +0100

    no more endlose loops in parser

--- src/generator/LevelParser.java
+++ src/generator/LevelParser.java
@@ -3,25 +3,16 @@ package generator;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
 import interfaces.*;
 import myGame.*;
 
-/* 
- * @author Andr� Matutat
- *
- */
 public class LevelParser {
 
-	/**
-	 * Parst ein kodiertes Level zu einer Instanz der Klasse Level
-	 * 
-	 * @param level kodiertes Level
-	 * @return erzeugtes Level
-	 */
-	public Level parseLevel(CodedLevel level) {
+	public Level parseLevel(final CodedLevel level) {
 		ISurface[][] lvl = new ISurface[level.getXSize()][level.getYSize()];
 
 		for (int x = 0; x < level.getXSize(); x++) {
@@ -34,84 +25,63 @@ public class LevelParser {
 					lvl[x][y] = new Start();
 				else if (level.getLevel()[x][y] == Constants.REFEERNCE_EXIT)
 					lvl[x][y] = new Exit();
-				
+
 			}
 		}
 
 		return new Level(level.getXSize(), level.getYSize(), lvl);
 	}
 
-	/**
-	 * Setzt ein Monster auf ein Zuf�lliges Surface
-	 * 
-	 * @param lvl            in welchem Level soll das Monster platziert werden
-	 * @param monster        Monster welches platziert werden soll
-	 * @param surfaceToPutOn auf welchen Surface das Monster platziert werden soll
-	 * @throws Exception
-	 */
-	public void placeMonster(Level lvl, Monster monster, ISurface surfaceToPutOn) throws Exception {
-		for (int i = 0; i < lvl.getXSize() * lvl.getYSize(); i++) {
+	public void placeMonster(final Level lvl, final Monster monster, final ISurface surfaceToPutOn) throws Exception {
+
+		ArrayList<ISurface> checkedSurfaces = new ArrayList<ISurface>();
+		while (checkedSurfaces.size() < lvl.getXSize() * lvl.getYSize()) {
 			int x = (int) Math.random() * lvl.getXSize();
 			int y = (int) Math.random() * lvl.getYSize();
-			if (lvl.getLevel()[x][y].getClass() == surfaceToPutOn.getClass()
+			if (!checkedSurfaces.contains(lvl.getLevel()[x][y])
+					&& lvl.getLevel()[x][y].getClass() == surfaceToPutOn.getClass()
 					&& lvl.getLevel()[x][y].setMonsterOnSurface(monster))
 				return;
+			else
+				checkedSurfaces.add(lvl.getLevel()[x][y]);
+
 		}
 		throw new Exception("Level voll");
 	}
 
-	/**
-	 * Setzt ein Item auf ein Zuf�lliges Surface
-	 * 
-	 * @param lvl            in welchem Level soll das Item platziert werden
-	 * @param item           Item welches platziert werden soll
-	 * @param surfaceToPutOn auf welchen Surface das Item platziert werden soll
-	 * @throws Exception
-	 */
-	public void placeItem(Level lvl, Item item, ISurface surfaceToPutOn) throws Exception {
+	public void placeItem(final Level lvl, final Item item, final ISurface surfaceToPutOn) throws Exception {
 
-		for (int i = 0; i < lvl.getXSize() * lvl.getYSize(); i++) {
+		ArrayList<ISurface> checkedSurfaces = new ArrayList<ISurface>();
+		while (checkedSurfaces.size() < lvl.getXSize() * lvl.getYSize()) {
 			int x = (int) Math.random() * lvl.getXSize();
 			int y = (int) Math.random() * lvl.getYSize();
-			if (x != 0 && y != 0 && x != lvl.getXSize() - 1 && y != lvl.getYSize() - 1
-					&& lvl.getLevel()[x][y].getClass() == surfaceToPutOn.getClass()
+			if (!checkedSurfaces.contains(lvl.getLevel()[x][y]) && x != 0 && y != 0 && x != lvl.getXSize() - 1
+					&& y != lvl.getYSize() - 1 && lvl.getLevel()[x][y].getClass() == surfaceToPutOn.getClass()
 					&& lvl.getLevel()[x][y].setItemOnSurface(item))
 				return;
+			else
+				checkedSurfaces.add(lvl.getLevel()[x][y]);
 		}
 		throw new Exception("Level voll");
 	}
 
-	/**
-	 * Wechselt ein zuf�lliges Surface aus
-	 * 
-	 * @param lvl        in welchem ein Surface ausgetauscht werden soll
-	 * @param newSurface neues Surface
-	 * @param oldSurface Typ des alten Surface
-	 * @throws Exception
-	 */
-	public void changeSurface(Level lvl, ISurface newSurface, ISurface oldSurface) throws Exception {
-		for (int i = 0; i < lvl.getXSize() * lvl.getYSize(); i++) {
+	public void changeSurface(final Level lvl, final ISurface newSurface, final ISurface oldSurfaceType)
+			throws Exception {
+		ArrayList<ISurface> checkedSurfaces = new ArrayList<ISurface>();
+		while (checkedSurfaces.size() < lvl.getXSize() * lvl.getYSize()) {
 			int x = (int) Math.random() * lvl.getXSize();
 			int y = (int) Math.random() * lvl.getYSize();
-			if (x != 0 && y != 0 && x != lvl.getXSize() - 1 && y != lvl.getYSize() - 1
-					&& lvl.getLevel()[x][y].getClass() == oldSurface.getClass()) {
+			if (!checkedSurfaces.contains(lvl.getLevel()[x][y]) && x != 0 && y != 0 && x != lvl.getXSize() - 1
+					&& y != lvl.getYSize() - 1 && lvl.getLevel()[x][y].getClass() == oldSurfaceType.getClass()) {
 				lvl.getLevel()[x][y] = newSurface;
 				return;
-			}
+			} else
+				checkedSurfaces.add(lvl.getLevel()[x][y]);
 		}
 		throw new Exception("Level voll");
 	}
 
-	/**
-	 * Erstellt die TexturenMap f�r ein Level
-	 * 
-	 * @param lvl  Level f�r welchem die TexturenMap generiert werden soll
-	 * @param path Pfad zum Speicherort an der die TexturenMap gespeichert werden
-	 *             soll
-	 * @param name Name der TexturenMap
-	 * @return ob das generieren erfolgreich war
-	 */
-	public boolean generateTextureMap(Level lvl, String path, String name) {
+	public boolean generateTextureMap(final Level lvl, final String path, final String name) {
 		try {
 			BufferedImage img1;
 			BufferedImage joinedImgLine = null;
